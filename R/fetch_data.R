e655c91f898f4b0e72ea881444a615411acb7483
Kelly Barnes <kbarnes@carpentries.org>
2023-09-25 11:43:59 -0400

    Revived fetch_airtable function

--- R/fetch_data.R
+++ R/fetch_data.R
@@ -12,16 +12,29 @@
 #'
 fetch_redash <- function(query_id) {
   api_key <- Sys.getenv(paste0("REDASH_QUERY_", query_id))
-  url <- paste0("https://redash.carpentries.org/api/queries/", query_id, "/results.csv?api_key=", api_key)
+  url <-
+    paste0(
+      "https://redash.carpentries.org/api/queries/",
+      query_id,
+      "/results.csv?api_key=",
+      api_key
+    )
   data <- utils::read.csv(url, stringsAsFactors = FALSE)
   return(data)
 }
 
+#' Fetch Airtable Data
+#'
+#' @param id
+#' @param db_name
+#'
+#' @return
+#' @export
+#'
+#' @examples
 fetch_airtable <- function(id, db_name) {
+  data <- airtabler::airtable(id, db_name)$`db_name`$select_all()
+#
+  return(data)
 
-# data <- airtabler::airtable(id, db_name)$`db_name`$select_all()
-
-# return(data)
-
-# }
-
+}
